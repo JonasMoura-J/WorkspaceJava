20cee5f7fce0eec4c8a7ca60675c91e36034bdc9
JonasMOura-J <jonasmourat9090@gmail.com>
2020-11-10 16:00:37 -0300

    Adicionando exemplo de abstract class no projeto

--- Account/src/entities/BusinessAccount.java
+++ Account/src/entities/BusinessAccount.java
@@ -1,4 +1,3 @@
-
 package entities;
 
 public class BusinessAccount extends Account {
@@ -26,5 +25,11 @@ public class BusinessAccount extends Account {
 		if (amount <= loanLimit) {
 			balance += amount - 10.0;
 		}
+	}
+	
+	@Override
+	public void withdraw(double amount) {
+		super.withdraw(amount);
+		balance -= 2.0;
 	}
 }
