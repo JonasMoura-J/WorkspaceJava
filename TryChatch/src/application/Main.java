348bf3c19773696e52d70af1ee20c601907b30d6
JonasMOura-J <jonasmourat9090@gmail.com>
2020-11-11 00:21:59 -0300

    Update no projeto TryCatch, adicionando exemplo com finally

--- TryChatch/src/application/Main.java
+++ TryChatch/src/application/Main.java
@@ -1,32 +1,49 @@
 package application;
 
-import java.util.InputMismatchException;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 	
-		Scanner sc = new Scanner(System.in);
+//		Scanner sc = new Scanner(System.in);
+//		
+//		try {
+//		String[] vect = sc.nextLine().split(" "); //lendo dados da mesma linha e separado no vetor pelos espaços em branco
+//		
+//		int position = sc.nextInt();
+//		System.out.println(vect[position]);
+//		
+//		}catch (ArrayIndexOutOfBoundsException e) {
+//			System.out.println("Posição inválida");
+//			
+//		}catch (InputMismatchException e) {
+//			System.out.println("Entrada inválida");
+//			e.printStackTrace();
+//		}
+//		
+//		System.out.println("Fim do programa");
+//		sc.close();
+		
+		File file = new File("/home/jonas/Documents/TesteJava.txt");
+		Scanner sc = null;
 		
 		try {
-		String[] vect = sc.nextLine().split(" "); //lendo dados da mesma linha e separado no vetor pelos espaços em branco
-		
-		int position = sc.nextInt();
-		System.out.println(vect[position]);
-		
-		}catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println("Posição inválida");
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
 			
-		}catch (InputMismatchException g) {
-			System.out.println("Entrada inválida");
+		}catch(FileNotFoundException e) {
+			System.out.println("Erro ao abrir arquivo " + e.getMessage());
+		
+		}finally {
+			
+			if(sc != null) {
+				sc.close();
+			}
 		}
-		
-		System.out.println("Fim do programa");
-		sc.close();
-		
 	}
-	
-	//InputMismatchException
-	//ArrayIndexOutOfBoundsException
 }
