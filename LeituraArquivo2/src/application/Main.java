7967a33539c50c1c679e721b5b5c2301bb10e6a5
JonasMoura-J <jonasmourat9090@gmail.com>
2020-11-12 14:37:35 -0300

    Update no projeto de leitura, para uma forma mais enxuta

--- LeituraArquivo2/src/application/Main.java
+++ LeituraArquivo2/src/application/Main.java
@@ -8,35 +8,50 @@ public class Main {
 
 	public static void main(String[] args) {
 		//implemetação mais "poluída" para leitura de arquivos
+//		String path = "test.txt";
+//		
+//		FileReader fr = null;
+//		BufferedReader bf = null;
+//		
+//		try {
+//			fr = new FileReader(path);
+//			bf = new BufferedReader(fr);
+//			String line = bf.readLine();
+//			
+//			while(line != null) {
+//				System.out.println(line);
+//				line = bf.readLine();
+//				
+//			}
+//		}catch(IOException e) {
+//			System.out.println("error" + e.getMessage());
+//		}finally {
+//			try {
+//				if(bf != null) {
+//					bf.close();
+//				}
+//				if(fr != null) {
+//					fr.close();
+//				}
+//			}catch(IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+		
+		
+		//forma mais enxuta
 		String path = "test.txt";
 		
-		FileReader fr = null;
-		BufferedReader bf = null;
-		
-		try {
-			fr = new FileReader(path);
-			bf = new BufferedReader(fr);
-			String line = bf.readLine();
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+			String line = br.readLine();
 			
 			while(line != null) {
 				System.out.println(line);
-				line = bf.readLine();
+				line = br.readLine();
 				
 			}
 		}catch(IOException e) {
 			System.out.println("error" + e.getMessage());
-		}finally {
-			try {
-				if(bf != null) {
-					bf.close();
-				}
-				if(fr != null) {
-					fr.close();
-				}
-			}catch(IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
-
 }
