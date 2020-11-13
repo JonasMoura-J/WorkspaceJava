15c92c302daa0c0077ff328acb0db5259952f1a6
JonasMoura-J <jonasmourat9090@gmail.com>
2020-11-13 11:19:50 -0300

    Segunda versao do projeto trabalhando com interfaces

--- Interfaces/src/application/Main.java
+++ Interfaces/src/application/Main.java
@@ -1,10 +1,52 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.AluguelCarros;
+import model.entities.Veiculo;
+import model.services.ImpostoServicoBrasil;
+import model.services.ServicoAluguel;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:ss");
+		
+		System.out.println("Digite os dados do aluguel: ");
+		System.out.print("Modelo do carro: ");
+		String modelo = sc.nextLine();
+		
+		System.out.print("Quando foi retirado (dd/MM/yyyy hh:ss): ");
+		Date inicio = sdf.parse(sc.nextLine());
+		
+		System.out.print("Quando retornou dd/MM/yyyy hh:ss): ");
+		Date fim = sdf.parse(sc.nextLine());
+		
+		AluguelCarros aluguel = new AluguelCarros(inicio, fim, new Veiculo(modelo));
+		
+		System.out.print("Digite o preço por hora: ");
+		double hora = sc.nextDouble();
+		
+		System.out.print("Digite o preço por dia: ");
+		double dia = sc.nextDouble();
+		
+		ServicoAluguel servico = new ServicoAluguel(dia, hora, new ImpostoServicoBrasil());
+		
+		servico.processarFatura(aluguel);
+		
+		System.out.println("FATURA: ");
+		System.out.printf("Pagamento basico: %.2f \n", aluguel.getFatura().getPagamentoBasico());
+		System.out.printf("Imposto: %.2f \n", aluguel.getFatura().getImposto());
+		System.out.printf("Pagamento total: %.2f \n", aluguel.getFatura().getPagamentoTotal());
+		
+		sc.close();
 	}
 
 }
--- Interfaces/src/model/services/ServicoAluguel.java
+++ Interfaces/src/model/services/ServicoAluguel.java
@@ -1,21 +1,35 @@
 package model.services;
 
 import model.entities.AluguelCarros;
+import model.entities.Fatura;
 
 public class ServicoAluguel {
 	private Double precoPorDia;
 	private Double precoPorHora;
 	
-	private ImpostoServicoBrasil imposto;
+	private ImpostoServicoBrasil servicoImposto;
 
 	public ServicoAluguel(Double precoPorDia, Double precoPorHora, ImpostoServicoBrasil imposto) {
 		super();
 		this.precoPorDia = precoPorDia;
 		this.precoPorHora = precoPorHora;
-		this.imposto = imposto;
+		this.servicoImposto = imposto;
 	}
 	
 	public void processarFatura(AluguelCarros aluguel) {
+		long t1 = aluguel.getInicio().getTime();
+		long t2 = aluguel.getFim().getTime();
 		
+		double hours = (double) (t2 - t1)/1000/60/60;
+		
+		double pagamentoBasico;
+		if (hours <= 12.0) {
+			pagamentoBasico = Math.ceil(hours) * precoPorHora;
+		}else {
+			pagamentoBasico = Math.ceil(hours/24) * precoPorDia;
+		}
+		double imposto = servicoImposto.imposto(pagamentoBasico);
+		
+		aluguel.setFatura(new Fatura(pagamentoBasico, imposto));
 	}
 }
