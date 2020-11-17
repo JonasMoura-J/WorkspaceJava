0d7aa9196be315c83d70df54c5e7555f616f6d58
JonasMoura-J <jonasmourat9090@gmail.com>
2020-11-16 21:59:11 -0300

    Startando o projeto

---
+++ IntroducaoGenerics/src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.Scanner;
+
+import services.PrintService;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		PrintService ps = new PrintService();
+		
+		System.out.print("Quantos valores irá digitar? ");
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			int value = sc.nextInt();
+			ps.addValue(value);
+			
+		}
+		
+		ps.print();
+		System.out.println("Primeiro digitado: " + ps.primeiro());
+		
+		sc.close();
+	}
+
+}
