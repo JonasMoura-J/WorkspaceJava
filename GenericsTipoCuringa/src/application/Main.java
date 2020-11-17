83f2ab00958de8cd5fc1ba7dd7895b2306b2ff12
JonasMoura-J <jonasmourat9090@gmail.com>
2020-11-17 09:28:30 -0300

    Adicionando exemplo de tipo curinga para listas

---
+++ GenericsTipoCuringa/src/application/Main.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
+		
+		List<String> myStrings = Arrays.asList("Jonas", "Maria", "Gabriel");
+		printList(myStrings);
+	}
+	
+	//List<?> acaba sendo uma lista de qualquer tipo
+	//porém não é possível adicionar elementos à lista
+	public static void printList(List<?> list) {
+		for(Object obj : list) {
+			System.out.println(obj);
+		}
+	}
+
+}
